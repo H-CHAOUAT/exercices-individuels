fe38a71824493cf4c222d5dbb451ad0f0a7cb24f
Hala CHAOUAT <hala.chaouat@gmail.com>
2024-02-16 15:45:53 +0100

    i finished the exercise

--- exo_2/index.js
+++ exo_2/index.js
@@ -41,10 +41,10 @@
 //     let givenNumber = prompt("Guess a number?")
 //      return givenNumber
 //  }
- 
+
 // function didIWin(giveANumber) {
 //       let number =22
-     
+
 //      if (giveANumber == number){
 //         return true 
 //      }
@@ -54,7 +54,7 @@
 // }
 
 // function gamePlay(){
-   
+
 //    let userGuess= parseInt(giveANumber())
 //    let newPhrase = didIWin(userGuess)
 //    let result =""
@@ -79,9 +79,9 @@
 //      return givenNumber
 //  }
 
- 
+
 // function didIWin(giveANumber, numberToGuess) {
-          
+
 //      if (giveANumber == numberToGuess){
 //         return true 
 //      }
@@ -107,40 +107,83 @@
 
 //etape 5:
 
-document.getElementById("secondPart").style.display="none"
+// document.getElementById("secondPart").style.display="none"
 
-document.getElementById("submit").addEventListener("click",function(){
-   let numberToGuess = document.getElementById("number1").value
-   document.getElementById("firstPart").value =""
-   document.body.innerHTML= ""
-   document.getElementById("secondPart").style.display = "inline-block"
+// document.getElementById("submit").addEventListener("click",function(){
+//    let numberToGuess = document.getElementById("number1").value
+//    document.getElementById("firstPart").style.display ="none"
+//    document.getElementById("secondPart").style.display = "block"
+//    let counter = 0
+//    document.getElementById("submit2").addEventListener("click", function(){
+//       let guessedNumber = document.getElementById("number2").value
+//       counter ++
+//       document.getElementById("secondPart").style.display = "none"
+//       if(numberToGuess == guessedNumber){
+//          document.getElementById("resultat").style.display= "block"
+//          document.getElementById("resultat").innerHTML =" You WON !!!!" + `you have succeeded after ${counter} attempts `
+//       }
+//       else{
+//          alert(`Try again!! You made ${counter} attempts`)
+//          document.getElementById("secondPart").style.display = "block"
+//       }
+
+//    })
+
+//    })
+
+//etape 6:
+let minimumValue = 0
+let maximumValue = 50  
+let range = document.body.innerHTML
+function firstPlayer() {
+   console.log('begin firstplayer function')
+   range = `The range of Value is between ${minimumValue} and ${maximumValue}`
+   //console.log(range)
+   document.getElementById("submit").addEventListener("click", function () {
+      // let numberToGuess = document.getElementById("number1").value
+      document.getElementById("firstPart").style.display = "none"
+      document.getElementById("secondPart").style.display = "block"
+      document.getElementById("resultat").style.display = "none"
+      secondPlayer()
    })
+}
+
+function secondPlayer() {
+   console.log('begin secondplayer function')
+   document.getElementById("range").innerHTML = `${range}`
+   document.getElementById("submit2").addEventListener("click", function () {
+      let guessedNumber = document.getElementById("number2").value
+    
+      document.getElementById("secondPart").style.display = "none"
+      document.getElementById("resultat").style.display = "none"
+      compare()
+   })
+ }
+let counter = 0
+
+function compare() {
+   counter++;
+   let numberToGuess = document.getElementById("number1").value;
+   let newGuess;
+
+   while (true) {
+       newGuess = document.getElementById("number2").value;
+
+       if (numberToGuess == newGuess) {
+           document.getElementById("resultat").style.display = "block";
+           document.getElementById("resultat").innerHTML = "You WON!!! You succeeded after " + counter + " attempts.";
+           break;
+       } else {
+           alert(`Try again!! You made ${counter} attempts`);
+           minimumValue = newGuess; // Update the minimumValue based on the newGuess
+           range = `The range of Value is between ${minimumValue} and ${maximumValue}`
+           console.log(range)
+           document.getElementById("range").innerHTML = `${range}`
+           document.getElementById("secondPart").style.display = "block";
+           break; // Break out of the loop after one attempt
+       }
+   }
+}
 
 
-// function gamePlay(){
-//       let numberToGuessByPlayer1 = numberToGuess()
-//       let isPlayerCorrect;
-//       let newGuess;
-   
-//         while (!isPlayerCorrect){
-//            newGuess = giveANumber()
-//             if (numberToGuessByPlayer1 == newGuess) {
-//                alert("You Won!")
-//                 break
-//             }
-//             }
-//         }
-//    gamePlay()
-   
-
-//  document.getElementById("submit").addEventListener("click",function(numberToGuess){
-//  let guessedNumber = document.getElementById("number").value
-//  let numberToGuessbyPlayer2 = numberToGuess()
-//  if( guessedNumber == numberToGuessbyPlayer2){
-//     document.body.innerHTML = ""
-//     document.body.innerHTML += "<h1>"+ "You Won" + "</h1>"
-//  }
-//  else{
-//     alert("try again")
-//  }
-//  })
+firstPlayer()
