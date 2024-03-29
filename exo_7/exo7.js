2b3e4e8f14fc48c1cc215f3e06a8873a8d2fb859
Hala CHAOUAT <hala.chaouat@gmail.com>
2024-03-29 09:49:40 +0100

    files are created

---
+++ exo_7/exo7.js
@@ -0,0 +1,7 @@
+// let matches= 50  
+// let number = prompt("enter a n")
+// function matchesRemoved(number){
+// return matches -= number
+// }
+// console.log(matchesRemoved(4))
+// console.log(matchesRemoved(4))
