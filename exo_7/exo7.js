a97aa0e36dc8c2b6cf61bbaff752e9257789b65d
Hala CHAOUAT <hala.chaouat@gmail.com>
2024-03-29 11:50:55 +0100

    first three steps are done

--- exo_7/exo7.js
+++ exo_7/exo7.js
@@ -1,7 +1,22 @@
-// let matches= 50  
-// let number = prompt("enter a n")
-// function matchesRemoved(number){
-// return matches -= number
-// }
-// console.log(matchesRemoved(4))
-// console.log(matchesRemoved(4))
+let totalMatches= 50  
+
+
+function matchesRemoved(numberOfMatchesRemoved){
+ totalMatches = totalMatches- numberOfMatchesRemoved
+ return totalMatches
+}
+
+function gameloop(){
+    while (totalMatches > 0) {
+        let numberOfMatchesRemoved =prompt("how many number of matches you want to remove ?")
+         if (numberOfMatchesRemoved >6 || numberOfMatchesRemoved < 1){
+             alert("enter a valid number between 1 and 6 ?")
+         }else{
+            matchesRemoved(numberOfMatchesRemoved)
+            alert(`you have ${totalMatches} matches left`)
+         }
+        }
+    return "you won"
+}  
+
+document.getElementById("result").innerHTML=gameloop()
